83b4bc92d845a051e542dde4376ccfb9d425fd34
mikeccy <mikeccy@gmail.com>
2013-02-15 18:41:01 -0500

    Added signature types

---
+++ main/cpp/com/mikeccy/scribe/model/SignatureType.h
@@ -0,0 +1,23 @@
+//
+//  SignatureType.h
+//  scribe-cpp
+//
+//  Created by mikeccy on 2/15/13.
+//  Copyright (c) 2013 mikeccy. All rights reserved.
+//
+
+#ifndef scribe_cpp_SignatureType_h
+#define scribe_cpp_SignatureType_h
+
+/**
+ * An enumeration containing the HTTP signature types.
+ *
+ * @author Chongyu Chen
+ */
+enum SignatureType
+{
+    Header,
+    QueryString
+};
+
+#endif

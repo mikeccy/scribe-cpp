413609ac9c0c6d44215ec4a035fccf9b1865910c
mikeccy <mikeccy@gmail.com>
2013-02-15 18:33:02 -0500

    Added verb enum for HTTP operations

---
+++ main/cpp/com/mikeccy/scribe/model/Verb.h
@@ -0,0 +1,22 @@
+//
+//  Verb.h
+//  scribe-cpp
+//
+//  Created by mikeccy on 2/14/13.
+//  Copyright (c) 2013 mikeccy. All rights reserved.
+//
+
+#ifndef scribe_cpp_Verb_h
+#define scribe_cpp_Verb_h
+
+/**
+ * An enumeration containing the most common HTTP Verbs.
+ *
+ * @author Chongyu Chen
+ */
+enum Verb
+{
+    GET, POST, PUT, DELETE, HEAD, OPTIONS, TRACE, PATCH
+};
+
+#endif
